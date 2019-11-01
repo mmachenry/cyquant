db0f55aba2ae58dc45c2bac246e1cc2c359233c4
Greg Echelberger <greg@keystonetowersystems.com>
2019-11-01 13:38:22 -0600

    Bump version: 1.0.0a6 → 1.0.0a7

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     author="Greg Echelberger",
     author_email="gechelberger@gmail.com",
     url="https://github.com/gechelberger/cyquant",
-    version="1.0.0a6",
+    version="1.0.0a7",
     description="cython dimensional analysis and unit tracking utility",
     packages=[package_name],
     cmdclass=CMDCLASS,
