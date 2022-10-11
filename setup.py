fba167cd53fd723ab3f936cce6b16ec15805d8eb
gsework <50334512+gsework@users.noreply.github.com>
2022-10-11 18:19:45 -0500

    bumpversion: v1.0.0

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     author="Greg Echelberger",
     author_email="gechelberger@gmail.com",
     url="https://github.com/gechelberger/cyquant",
-    version="1.0.0a10",
+    version="1.0.0",
     description="cython dimensional analysis and unit tracking utility",
     packages=[package_name],
     cmdclass=CMDCLASS,
